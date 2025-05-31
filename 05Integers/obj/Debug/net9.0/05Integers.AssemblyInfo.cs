6fdfbd4f90a78e6910b81528f77adcdff5b020d1
MrAWHolmes <archie.holmes@gmail.com>
2025-05-31 16:31:55 +0100

    SAT PUSH PRE-WORK

--- 05Integers/obj/Debug/net9.0/05Integers.AssemblyInfo.cs
+++ 05Integers/obj/Debug/net9.0/05Integers.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("05Integers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83b863d44017da364338423cb2106badc99dd520")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f28ed945757aea9adea177ddaf83c77a8500f7a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("05Integers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("05Integers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
